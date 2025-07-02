a3af85ef059af523aa5396739d7ef09b7aa69b29
Sam Warren <sam@junctiontech.ca>
2025-07-02 16:58:50 -0700

    Tweak theme

--- components/navigation/site-header.tsx
+++ components/navigation/site-header.tsx
@@ -11,10 +11,6 @@ export function SiteHeader() {
           className="mx-2 data-[orientation=vertical]:h-4"
         />
         <h1 className="text-base font-medium">Dashboard</h1>
-
-        {/* <div className="ml-auto flex items-center gap-2">
-          <ModeToggle />
-        </div> */}
       </div>
     </header>
   );
